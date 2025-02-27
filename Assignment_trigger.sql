134f506b494f4cf3057a1a01227342686a4659fe
Ashwini Patil <121940158+fhalguni@users.noreply.github.com>
2025-02-27 14:07:44 +0530

    Add files via upload

---
+++ Assignment_trigger.sql
@@ -0,0 +1,171 @@
+create table Customers2(
+	customerId int primary key,
+	customerName varchar(30),
+	email varchar(30),
+	PhoneNumber varchar(10)
+);
+
+
+
+INSERT INTO Customers2 (customerId, customerName, email, PhoneNumber) VALUES
+(1, 'John Doe', 'john.doe@example.com', '1234567890'),
+(2, 'Jane Smith', 'jane.smith@example.com', '0987654321'),
+(3, 'Alice Johnson', 'alice.johnson@example.com', '5555555555');
+
+--------------------------------------------------------------------------------------------------------------------------------------
+create table Accounts2(
+	AccountId int primary key,
+	customerId int,
+	AccountNumber varchar(20),
+	Balance decimal,
+	AccountType varchar(30),
+	foreign key (customerId) references Customers2(customerId)
+);
+
+INSERT INTO Accounts2 (AccountId, customerId, AccountNumber, Balance, AccountType) VALUES
+(1, 1, 'ACC123456', 1000.50, 'Savings'),
+(2, 2, 'ACC654321', 2500.75, 'Checking'),
+(3, 3, 'ACC987654', 1500.00, 'Savings');
+truncate table Accounts2;
+select * from Accounts2;
+------------------------------------------------------------------------------------------------------------------------------------------
+create table Transactions1(
+	transactionId int primary key,
+	AccountId int,
+	transactionType varchar(30),
+	Amount decimal,
+	transactionDate date,
+	foreign key (AccountId) references Accounts2(AccountId)
+);
+
+INSERT INTO Transactions1 (transactionId, AccountId, transactionType, Amount, transactionDate) VALUES
+(1, 1, 'Deposit', 500.00, '2025-02-01'),
+(2, 2, 'Withdrawal', 200.00, '2025-02-05'),
+(3, 3, 'Deposit', 300.00, '2025-02-10');
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+create table Audit_Transactions1(
+	AuditId int primary key,
+	AccountId int,
+	Amount decimal,
+	transactionDate date,
+	Action1 varchar(30),
+	foreign key (AccountId) references Accounts2(AccountId)
+);
+select * from Accounts2;
+INSERT INTO Audit_Transactions1 (AuditId, AccountId, Amount, transactionDate, Action1) VALUES
+(1, 1, 500.00, '2025-02-01', 'Deposit'),
+(2, 2, 200.00, '2025-02-05', 'Withdrawal'),
+(3, 3, 300.00, '2025-02-10', 'Deposit');
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+create clustered index account_index on Accounts2(AccountId);
+
+create nonclustered index customer_index on Customers2(customerName);
+
+create index composite_index on Transactions1(transactionDate,Amount);
+
+create unique index unique_index on Accounts2(AccountNumber);
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+create function getCalculateInterest(@AccountId int)
+returns decimal(10,2) as
+begin
+declare @interest decimal(10,2)
+select  @interest= (Balance-Balance*0.05) from Accounts2 where AccountId=@AccountId
+return @interest
+end;
+
+drop function getCalculateInterest;
+
+select dbo.getCalculateInterest(1) as Interest;
+------------------------------------------------------------------------------------------------------------------------------------------
+
+drop procedure TransferAmount;
+--truncate table trasaction_Detail4;
+create table trasaction_Detail4(
+	fromAccount int,
+	toAccount int,
+	amount decimal,
+	transactionDate date
+);
+
+create Procedure TransferAmount 
+	@FromAccountId int,
+	@ToAccountId int,
+	@Amount decimal
+As
+Begin
+	declare @Balance decimal
+	
+	select @Balance=Balance from Accounts2 where AccountId=@FromAccountId;
+	print @Balance
+	print @Amount
+	if @Balance is null
+	begin
+	rollback
+	print 'Balance is null'
+	return
+	end
+
+	if @Balance < @Amount
+	begin
+	rollback
+	print 'Insufficient balance'
+	return 
+	end
+
+	update Accounts2 set Balance=Balance-@Amount where AccountId = @FromAccountId;
+
+	update Accounts2 set Balance=Balance+@Amount where AccountId = @ToAccountId;
+
+	insert into trasaction_Detail4 values(@FromAccountId,@ToAccountId,@Amount, getdate())
+
+	commit;
+	end;
+
+	exec TransferAmount 2,3,500.00;
+
+	select * from trasaction_Detail4;
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+CREATE TRIGGER prevent_insufficient_withdrawal
+ON Accounts2
+AFTER UPDATE
+AS
+BEGIN
+    IF EXISTS (SELECT * FROM inserted WHERE Balance < 0)
+    BEGIN
+        RAISERROR ('Insufficient Balance', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+
+END;
+
+update Accounts2 set Balance=balance-10000 where AccountId=3;
+
+---------------------------------------------------------------------------------------------------------------------------------------
+--Task 5
+CREATE TRIGGER AuditTransactions2 ON Transactions1
+AFTER INSERT
+AS 
+BEGIN
+	declare @AuditId int=6, @AccountId int, @Amount decimal,@transactionDate date, @Action1 varchar(30)='Deposite'
+	SELECT @AccountId = AccountId, 
+           @Amount = Amount, 
+           @transactionDate = transactionDate 
+    FROM inserted;
+    INSERT INTO Audit_Transactions1 (AuditId, AccountId, Amount, transactionDate, Action1)values(@AuditId, @AccountId, @Amount, @transactionDate, @Action1) 
+     
+END;
+
+
+
+INSERT INTO Transactions1 (transactionId, AccountId, transactionType, Amount, transactionDate)
+VALUES (6, 1, 'Withdrawal', 500.00, GETDATE());
+
+
+drop trigger AuditTransactions1;
+SELECT * FROM Audit_Transactions1;
---
+++ join.sql
@@ -0,0 +1,167 @@
+CREATE TABLE Departments4 (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(100)
+);
+
+CREATE TABLE Employees10 (
+    EmployeeID INT PRIMARY KEY,
+    EmployeeName VARCHAR(100),
+    DepartmentID INT,
+    HireDate DATE,
+    BaseSalary DECIMAL(10,2),
+    Bonus DECIMAL(10,2),
+    ManagerID INT,
+    FOREIGN KEY (DepartmentID) REFERENCES Departments4(DepartmentID),
+    FOREIGN KEY (ManagerID) REFERENCES Employees10(EmployeeID)
+);
+
+-- Inserting sample data
+INSERT INTO Departments4 (DepartmentID, DepartmentName) VALUES
+(1, 'HR'), (2, 'IT'), (3, 'Finance');
+
+
+
+select * from Departments4;
+INSERT INTO Employees10 (EmployeeID, EmployeeName, DepartmentID, HireDate, BaseSalary, Bonus, ManagerID) VALUES
+(1, 'Alice', 1, '2020-01-10', 50000, 5000, NULL),
+(2, 'Bob', 2, '2019-03-15', 60000, 6000, 1),
+(3, 'Charlie', 2, '2018-06-20', 55000, 5500, 1),
+(4, 'David', 3, '2021-07-25', 70000, 7000, 2);
+
+select * from Employees10;
+
+--Find Employees and Their Department Names Using INNER JOIN
+SELECT 
+    Employees10.EmployeeName, 
+    Departments4.DepartmentName, 
+    Employees10.HireDate
+FROM 
+    Employees10
+INNER JOIN 
+    Departments4 ON Employees10.DepartmentID = Departments4.DepartmentID;
+
+--List Employees Without a Manager (SELF JOIN or LEFT JOIN):
+
+	SELECT 
+    e1.EmployeeName
+FROM 
+    Employees10 e1
+LEFT JOIN 
+    Employees10 e2 ON e1.ManagerID = e2.EmployeeID
+WHERE 
+    e1.ManagerID IS NULL;
+
+
+--Find Departments Without Any Employees (LEFT JOIN)
+insert into Employees10 values (5, 'Bob', null, '2019-03-15', 60000, 6000, 1)
+SELECT 
+    Departments4.DepartmentName
+FROM 
+    Departments4
+LEFT JOIN 
+    Employees10 ON Departments4.DepartmentID = Employees10.DepartmentID
+WHERE 
+    Employees10.EmployeeID IS NULL;
+
+
+--Get the Total Salary (Base + Bonus) for Each Employee (JOIN Calculation)
+
+SELECT 
+    EmployeeName, 
+    (BaseSalary + Bonus) AS TotalSalary
+FROM 
+    Employees10;
+
+
+--Identify the Employee with the Highest Salary (Subquery)
+SELECT 
+    EmployeeName
+FROM 
+    Employees10
+WHERE 
+    (BaseSalary + Bonus) = (
+        SELECT 
+            MAX(BaseSalary + Bonus)
+        FROM 
+            Employees10
+    );
+
+--Find Employees Earning More Than Their Manager (SELF JOIN)
+SELECT 
+    e1.EmployeeName
+FROM 
+    Employees10 e1
+INNER JOIN 
+    Employees10 e2 ON e1.ManagerID = e2.EmployeeID
+WHERE 
+    e1.BaseSalary + e1.Bonus > e2.BaseSalary + e2.Bonus;
+
+--Create a View for HR to See Employee Salary Details
+CREATE VIEW SalaryView AS
+SELECT 
+    e.EmployeeID, 
+    e.EmployeeName, 
+    d.DepartmentName, 
+    e.BaseSalary, 
+    e.Bonus, 
+    (e.BaseSalary + e.Bonus) AS TotalSalary
+
+from Employees10 e
+join Departments4 d on e.DepartmentID=d.DepartmentID
+
+select * from SalaryView;
+
+
+select * from Employees10 
+where DATEDIFF(curdate() , HireDate) >3 *365;
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+create function getTotalSalary(@BaseSalary DECIMAL(10,2)) 
+returns DECIMAL(10,2) as
+
+begin 
+ return @BaseSalary*0.10 ;
+end;
+
+select EmployeeID,EmployeeName, dbo.getTotalSalary(BaseSalary) As Bonus from Employees10;
+
+create function getDepartment(@DeptId int)
+returns TABLE as
+return 
+
+(select EmployeeID,EmployeeName from Employees10 where DepartmentID=@DeptId);
+;
+
+select * from dbo.getDepartment(1);
+------------------------------------------------------------------------------------------------------------------------------
+
+create procedure addEmployee 
+	@EmployeeID INT ,
+    @EmployeeName VARCHAR(100),
+    @DepartmentID INT,
+    @HireDate DATE,
+    @BaseSalary DECIMAL(10,2),
+    @Bonus DECIMAL(10,2),
+    @ManagerID INT
+  AS
+  begin 
+  insert into Employees10 values (@EmployeeID,@EmployeeName,@DepartmentID,@HireDate,@BaseSalary,@Bonus,@ManagerID);
+  end;
+
+  exec addEmployee 5, 'Ashu', 3, '2021-07-25', 70000, 7000, 2;
+  select * from Employees10;
+
+  create trigger onDelete
+  on Employees10
+  instead of delete
+  as
+  begin
+  update Employees10
+  set HireDate=null, DepartmentID=null
+  where EmployeeID in(select EmployeeID from deleted)
+  end;
+
+  delete from Employees10 where EmployeeID=1;
+
+  select * from Employees10;
